9f4d182646d0b6cf7a8a41fb051674ea7e056d3e
Dietmar Schabus <dietmar.schabus@gmail.com>
2018-04-30 09:11:08 +0200

    create PDFs

--- randomized_multiple_choice_exam/create_exams.py
+++ randomized_multiple_choice_exam/create_exams.py
@@ -1,9 +1,15 @@
 import argparse
 import csv
+import os
 import random
+import subprocess
 import sys
 
 from jinja2 import Environment, FileSystemLoader
+
+ROOTDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+TEXDIR = 'latex'
+PDFDIR = 'pdf'
 
 def parse_error(n, filename, row):
     print('PARSE ERROR: Line %d of file "%s" seems problematic ("%s")' %
@@ -41,11 +47,15 @@ def read_questions(filename):
                         parse_error(i+1, filename, row)
             else:
                 parse_error(i+1, filename, row)
+        if q:
+            questions.append(q)
     return questions
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', help='Number of exams to generate')
+    parser.add_argument('-t', '--title', default='')
+    parser.add_argument('-n', type=int, default=1,
+        help='Number of exams to generate')
     parser.add_argument('--seed', type=int, default=None, help='Random seed')
     parser.add_argument('questions_file')
     args = parser.parse_args()
@@ -53,15 +63,39 @@ if __name__ == '__main__':
     questions = read_questions(args.questions_file)
     random.seed(args.seed)
 
-    shuffled = questions[:]
-    random.shuffle(shuffled)
-    for i, q in enumerate(shuffled):
-        q['idx'] = i + 1
-        q['answers'] = q['correct'] + q['wrong']
-        random.shuffle(q['answers'])
-
+    # http://eosrei.net/articles/2015/11/latex-templates-python-and-jinja2-generate-pdfs
     env = Environment(
-        loader=FileSystemLoader('templates'),
+    	block_start_string = '\BLOCK{',
+    	block_end_string = '}',
+    	variable_start_string = '\VAR{',
+    	variable_end_string = '}',
+    	comment_start_string = '\#{',
+    	comment_end_string = '}',
+    	line_statement_prefix = '%%',
+    	line_comment_prefix = '%#',
+    	trim_blocks = True,
+    	autoescape = False,
+        loader=FileSystemLoader(os.path.join(ROOTDIR, 'templates')),
     )
-    tpl = env.get_template('template.tex')
-    print(tpl.render(questions=shuffled))
+    if not os.path.exists(TEXDIR):
+        os.mkdir(TEXDIR)
+
+    if not os.path.exists(PDFDIR):
+        os.mkdir(PDFDIR)
+
+    for i in range(1, args.n + 1):
+        shuffled = questions[:]
+        random.shuffle(shuffled)
+        for j, q in enumerate(shuffled):
+            q['idx'] = j + 1
+            q['answers'] = q['correct'] + q['wrong']
+            random.shuffle(q['answers'])
+
+        # generate .tex files
+        tpl = env.get_template('template.tex')
+        texfile = os.path.join(TEXDIR, 'exam_%d.tex' % i)
+        with open(texfile, 'w') as f:
+            f.write(tpl.render(questions=shuffled, title=args.title))
+
+        # compile to PDF
+        subprocess.call(['pdflatex', '-output-directory', PDFDIR, texfile])
